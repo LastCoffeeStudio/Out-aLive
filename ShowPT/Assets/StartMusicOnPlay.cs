3cef55567914f2f0991ccaa6993e36df3a35b4bd
Carlos <carlos.2380@hotmail.com>
2018-04-23 12:33:45 +0200

    [LCS-77] Musica en el juego.

---
+++ ShowPT/Assets/StartMusicOnPlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMusicOnPlay : MonoBehaviour {
+
+	// Use this for initialization
+    public  AudioClip music;
+	void Start () {
+	    gameObject.GetComponent<CtrlAudio>().playOneSound("Music", music, transform.position, 1.0f, 0.0f, 100);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
