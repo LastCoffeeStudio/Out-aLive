fa544b68a0b5ddf2fc17b6fc26fa6ea4175cd81f
Casa\Adrián Meizoso <trucuta5@hotmail.com>
2018-06-24 01:46:05 +0200

    Locked door sound added

---
+++ ShowPT/Assets/Scripts/InteractableDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableDoor : InteractableObject
+{
+    private CtrlAudio ctrlAudio;
+    public AudioCollection lockedDoor;
+
+    private ulong idClip = 0;
+
+    protected override void Start()
+    {
+        ctrlAudio = GameObject.FindGameObjectWithTag("CtrlAudio").GetComponent<CtrlAudio>();
+        base.Start();
+    }
+
+    protected override void executeAction()
+    {
+        ctrlAudio.stopSound(idClip);
+        idClip = ctrlAudio.playOneSound(lockedDoor.audioGroup, lockedDoor[0], transform.position, lockedDoor.volume, lockedDoor.spatialBlend, lockedDoor.priority);
+    }
+}
