f356dbeae1392a37e28059b156da76b26cb686b7
Carlos <carlos.2380@hotmail.com>
2018-08-21 19:35:23 +0200

    First iteration of event drones.

--- ShowPT/Assets/Scripts/CtrlShieldDrones.cs
+++ ShowPT/Assets/Scripts/CtrlShieldDrones.cs
@@ -8,6 +8,7 @@ public class CtrlShieldDrones : MonoBehaviour {
     public float radioHomeEnter;
     //Distance betwen Target and player for drones comeback to target.
     public float radioHomeExit;
+    [Header("Shield Behaivor, movment and speed")]
     public float radioSeparation;
     public float KSeparation;
     public float KPlayer;
@@ -20,12 +21,13 @@ public class CtrlShieldDrones : MonoBehaviour {
     private GameObject Snitch;
     private GameObject sphere;
     private int dronesAlive;
-
+    private List<GameObject> shieldsObjects;
     // Use this for initialization
     void Start ()
     {
         dronesAlive = 0;
         shieldDrones = new List<AIShieldDrone>();
+        shieldsObjects = new List<GameObject>();
         player = GameObject.FindGameObjectWithTag("Player");
         
         for (int i = 0; i < transform.childCount; ++i)
@@ -35,32 +37,48 @@ public class CtrlShieldDrones : MonoBehaviour {
                 transform.GetChild(i).GetComponent<AIShieldDrone>().player = player;
                 transform.GetChild(i).GetComponent<AIShieldDrone>().ctrlShieldDrones = this;
                 shieldDrones.Add(transform.GetChild(i).GetComponent<AIShieldDrone>());
+                shieldsObjects.Add(transform.GetChild(i).gameObject);
                 ++dronesAlive;
             }else if (transform.GetChild(i).name == "Snitch")
             {
                 Snitch = transform.GetChild(i).gameObject;
+                Snitch.transform.parent = null;
             }
             else if (transform.GetChild(i).name == "Sphere")
             {
                 sphere = transform.GetChild(i).gameObject;
             }
         }
+        for (int i = 0; i < shieldsObjects.Count; i++)
+        {
+            shieldsObjects[i].transform.GetChild(0).Translate(0f, 0f, 10f);
+        }
+        transform.Translate(0f, 30f, 0f);
     }
 
+    bool trigerFinish = false;
     private void Update()
     {
+        if (trigerFinish == false)
+        {
+            
+            StartCoroutine(closeDoorsSmooth());
+            trigerFinish = true;
+        }
         if (playerInHome == false)
         {
             if (Vector3.Distance(player.transform.position, transform.position) <= radioHomeEnter)
             {
-                playerInHome = true;
+                //playerInHome = true;
             }
+
+          
         }
         else
         {
             if (Vector3.Distance(player.transform.position, transform.position) >= radioHomeExit)
             {
-                playerInHome = false;
+                //playerInHome = false;
             }
         }
         
@@ -92,6 +110,32 @@ public class CtrlShieldDrones : MonoBehaviour {
         {
             sphere.SetActive(false);
             Snitch.GetComponent<SphereCollider>().enabled = true;
+        }
+    }
+
+    public AnimationCurve curve;
+    public float timeClosing = 5f;
+    IEnumerator closeDoorsSmooth()
+    {
+        float time = 0f;
+        float speed = -3f;
+        float angleSpeed = 30f;
+        Vector3 startRotation = transform.position;
+        Vector3 endRotation = Snitch.transform.position;
+        Vector3 startPosition = shieldsObjects[0].transform.GetChild(0).transform.localPosition;
+        Vector3 endPosition = startPosition;
+        endPosition.z = 3f;
+        while (time <= timeClosing)
+        {
+            time += Time.deltaTime;
+            float t = time / timeClosing;
+            transform.position = Vector3.Lerp(startRotation, endRotation, curve.Evaluate(t));
+            for (int i = 0; i < shieldsObjects.Count; i++)
+            {
+                shieldsObjects[i].transform.Rotate(0f, angleSpeed*Time.deltaTime, 0f);
+                shieldsObjects[i].transform.GetChild(0).transform.localPosition = Vector3.Lerp(startPosition, endPosition, curve.Evaluate(t));
+            }
+            yield return null;
         }
     }
 }
