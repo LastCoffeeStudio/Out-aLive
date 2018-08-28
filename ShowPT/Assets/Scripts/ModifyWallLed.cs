1e36dbfd9a7348502fac45b5fc5ba5021526ae84
Carlos <carlos.2380@hotmail.com>
2018-08-28 21:50:48 +0200

    Leds are more visible (background are black) in the walls when player are near.

---
+++ ShowPT/Assets/Scripts/ModifyWallLed.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyWallLed : MonoBehaviour
+{
+
+    public float rango;
+    private Transform playerTransform;
+    private Renderer rend ;
+    // Use this for initialization
+    void Start ()
+	{
+	    playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+	    rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (playerTransform.position.x < (-63.8f + rango))
+	    {
+	        Color c = rend.material.color;
+	        c.r = 1 - ((-((playerTransform.position.x) - (-63.8f + rango))) / rango);
+	        if (c.r > 1)
+	        {
+	            c.r = 1;
+
+	        }
+	        else if (c.r < 0)
+	        {
+	            c.r = 0;
+
+	        }
+	        c.g = c.b = c.r;
+	        rend.material.SetColor("_Color", c);
+	    }
+
+    }
+}
---
+++ ShowPT/Assets/Scripts/ReloadStateBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadStateBehaviour : StateMachineBehaviour {
+
+    private Gun gun;
+
+    // This will be called when the animator first transitions to this state.
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        gun = GameObject.FindGameObjectWithTag("Gun").GetComponent<Gun>();
+    }
+
+    // This will be called once the animator has transitioned out of the state.
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        gun.endReload();
+    }
+
+    // This will be called every frame whilst in the state.
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
