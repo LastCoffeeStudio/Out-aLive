398eee286abfc3e0c89b01aae898406402b2a488
Casa\Adrián Meizoso <trucuta5@hotmail.com>
2018-09-13 19:35:44 +0200

    Deleted raycast from shotgun

--- ShowPT/Assets/Scripts/Shotgun.cs
+++ ShowPT/Assets/Scripts/Shotgun.cs
@@ -28,7 +28,6 @@ public class Shotgun : Weapon
 
     protected override void shotBullet(Ray ray)
     {
-        base.shotBullet(ray);
         GameObject projectile = Instantiate(projectileToShoot, shootPoint.position, Quaternion.LookRotation(Vector3.Normalize((ray.origin + ray.direction * weaponRange) - shootPoint.position)), shootPoint);
         projectile.transform.Rotate(0f, 180f, 0f);
     }
