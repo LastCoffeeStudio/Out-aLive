54ea57e7db1c3deebe8c4dff1d274f80ba88410a
Carlos <carlos.2380@hotmail.com>
2018-09-17 00:35:41 +0200

    Good effect decal

--- ShowPT/Assets/Graphical Resources/Images/DestroyDecalLed.cs
+++ ShowPT/Assets/Graphical Resources/Images/DestroyDecalLed.cs
@@ -7,16 +7,26 @@ public class DestroyDecalLed : MonoBehaviour
 
     public float speedDesappear;
     public float secondsLife;
+
+    private float size;
     // Update is called once per frame
     private void Start()
     {
+        size = transform.localScale.x;
+        transform.localScale = Vector3.zero;
         StartCoroutine(destryMe());
     }
 
     private IEnumerator destryMe()
     {
-        yield return new WaitForSeconds(secondsLife);
         float scale = speedDesappear * Time.deltaTime;
+        while (transform.localScale.x < size)
+        {
+            transform.localScale += new Vector3(scale, scale, scale);
+            yield return null;
+        }
+        yield return new WaitForSeconds(secondsLife);
+
 
         while (transform.localScale.x > 0.001)
         {
