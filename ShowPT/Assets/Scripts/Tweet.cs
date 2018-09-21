fa0d0c53e7eb796d3222b460d00eddaa1eb21529
lordmizel <lordmizell@gmail.com>
2018-09-21 03:28:47 +0200

    First iteration of what a tweet might look like.

---
+++ ShowPT/Assets/Scripts/Tweet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Tweet : ScriptableObject 
+{
+	public Tweeter tweeter;
+	public string tweetText;
+}
