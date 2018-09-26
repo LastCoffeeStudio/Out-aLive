e8fa1e92fffb5fcb51b72197e682811651e95835
Carlos <carlos.2380@hotmail.com>
2018-09-27 01:32:01 +0200

    Sounds in hall

---
+++ ShowPT/Assets/Scripts/FridgeInteractable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeInteractable : InteractableObject
+{
+    public AudioClip clip;
+    private CtrlAudio ctrlAudio;
+
+    private ulong idClip = 0;
+
+    protected override void Start()
+    {
+        ctrlAudio = GameObject.FindGameObjectWithTag("CtrlAudio").GetComponent<CtrlAudio>();
+        base.Start();
+    }
+
+    protected override void executeAction()
+    {
+        ctrlAudio.stopSound(idClip);
+        idClip = ctrlAudio.playOneSound("Scene", clip, transform.position, 0.5f, 0f, 128);
+    }
+}
