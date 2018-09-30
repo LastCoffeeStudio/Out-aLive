6d616d74c43dc728463a2c2cd70774467185a205
Carlos <carlos.2380@hotmail.com>
2018-09-30 04:06:23 +0200

    Hud appear with animation

--- ShowPT/Assets/ReturnControlPlayer.cs
+++ ShowPT/Assets/ReturnControlPlayer.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReturnControlPlayer : MonoBehaviour {
-    
+public class ReturnControlPlayer : MonoBehaviour
+{
+    private GameObject HUD;
+
 	void Start ()
 	{
 	    CtrlGameState.gameState = CtrlGameState.gameStates.INITIALINTRO;
         GameObject sceneUI = GameObject.FindGameObjectWithTag("SceneUI");
-	    sceneUI.GetComponentInChildren<HudController>().gameObject.SetActive(false);
+	    HUD = sceneUI.GetComponentInChildren<HudController>().gameObject;
+        HUD.SetActive(false);
         PlayerMovment.overrideControls = true;
     }
 
     public void returnToMove()
     {
+        HUD.SetActive(true);
         CtrlGameState.gameState = CtrlGameState.gameStates.ACTIVE;
         PlayerMovment.overrideControls = false;
     }
