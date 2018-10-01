e00710e93c7d2313d40dccad6364066f9afc3642
Carlos <carlos.2380@hotmail.com>
2018-10-01 03:55:06 +0200

    Better sound for bridge

--- ShowPT/Assets/Scripts/SnitchEnemy.cs
+++ ShowPT/Assets/Scripts/SnitchEnemy.cs
@@ -48,8 +48,7 @@ public class SnitchEnemy : Enemy {
             ctrAudio.stopSound(idSnichSound);
             ScoreController.addDead(ScoreController.Enemy.DRON);
 			bridge.SetActive (true);
-            ctrAudio.playOneSound("Scene", brigeSound, bridge.transform.position, 0.4f, 0f, 50);
-            ctrAudio.playOneSound("Scene", brigeSound, bridge.transform.position, 1f, 1f, 50);
+            ctrAudio.playOneSound("Scene", brigeSound, bridge.transform.position, 1f, 1f, 100, false, null, 50F, 0F, AudioRolloffMode.Linear);
             tVShowmanManager.playMessageAllTVs(tvs, GenericEvent.EventType.BRIDGE);
             //ctrAudio.playOneSound(tvCollection.audioGroup, tvCollection[(int)GenericEvent.EventType.BRIDGE], Vector3.zero, 0.05f, 0f, tvCollection.priority);
             generateDeathEffect ();
