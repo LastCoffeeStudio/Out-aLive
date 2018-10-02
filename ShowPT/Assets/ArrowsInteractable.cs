1727a01f6a969baa770879bca629dc7dcf4c3de3
Adrián Meizoso <trucuta5@hotmail.com>
2018-10-02 15:30:32 +0200

    Anim to machine arrow added

---
+++ ShowPT/Assets/ArrowsInteractable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowsInteractable : InteractableObject {
+
+    private CtrlAudio ctrlAudio;
+    public AudioClip audioClip;
+    public int direcction;
+    public ResourceMachineController resourceMachineController;
+
+    protected override void Start()
+    {
+        ctrlAudio = GameObject.FindGameObjectWithTag("CtrlAudio").GetComponent<CtrlAudio>();
+        base.Start();
+    }
+
+    protected override void executeAction()
+    {
+        ctrlAudio.playOneSound("UI", audioClip, transform.position, 1.0f, 0f, 128);
+        resourceMachineController.updateSelected(direcction);
+    }
+}
