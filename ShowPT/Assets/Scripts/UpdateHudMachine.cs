8b53c20699b036da2157e20993c8c8af72001aba
Carlos <carlos.2380@hotmail.com>
2018-10-03 04:16:42 +0200

    Fixed lock first weapond in machine

---
+++ ShowPT/Assets/Scripts/UpdateHudMachine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateHudMachine : MonoBehaviour
+{
+    private ResourceMachineController resourceMachineController;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    resourceMachineController = transform.GetComponentInChildren<ResourceMachineController>();
+	}
+	
+	// Update is called once per frame
+	public void updateHUD () {
+	    resourceMachineController.updateSelected(0);
+    }
+}
