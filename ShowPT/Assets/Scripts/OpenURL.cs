ef1017cf43df7f40581e72b85c9436c7a0560944
Carlos <carlos.2380@hotmail.com>
2018-10-13 02:41:15 +0200

    LinkedIn credits

---
+++ ShowPT/Assets/Scripts/OpenURL.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+
+    public string url;
+
+    public void OpenUrl()
+    {
+        Application.OpenURL(url);
+    }
+}
